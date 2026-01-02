ffecd6b29600b628abd296e5046dc67d1c8cb6cc
Dilagante <dinethdds@gmail.com>
2026-01-02 15:08:38 +0530

    chore: install react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import TopSection from "./components/Hero";
+import Hero from "./components/Hero";
 import PropertyList from "./components/PropertyList";
 import propertiesData from "./data/properties.json";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -16,7 +16,7 @@ function App() {
         <Header />
 
         <main>
-          <TopSection />
+          <Hero />
           <Container fluid style={{padding: "2rem", background: "#f9fafb",minHeight: "100vh",}}
           >
             <h1 style={{textAlign: "center", marginBottom: "2rem", color: "#111827",}}>
