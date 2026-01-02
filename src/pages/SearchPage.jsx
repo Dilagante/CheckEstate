9dd93a80ae6a659ea073eff5e2faa5483aee8888
Dilagante <dinethdds@gmail.com>
2026-01-02 15:11:31 +0530

    feat: create "Search" page for router
    - Note: currently unused

---
+++ src/pages/SearchPage.jsx
@@ -0,0 +1,43 @@
+import { Container } from 'react-bootstrap'
+import Header from '../components/Header/Header'
+import Hero from '../components/Hero/Hero'
+import PropertyList from '../components/PropertyList/PropertyList'
+import propertiesData from '../data/properties.json'
+import '../styles/SearchPage.css'
+
+
+function SearchPage() {
+  return (
+    <div className="search-page">
+      {/* Header/Navbar */}
+      <Header favouritesCount={0} />
+
+      {/* Hero Section */}
+      <Hero />
+
+      {/* Main Content */}
+      <section className="search-page__content">
+        <Container fluid>
+          {/* Search form will go here later */}
+          <div className="search-form-placeholder">
+            <p>🔍 Search form coming soon...</p>
+          </div>
+
+          {/* Property Results */}
+          <div className="search-page__results">
+            <PropertyList properties={propertiesData.properties} />
+          </div>
+        </Container>
+      </section>
+
+      {/* Footer (optional for now) */}
+      <footer className="search-page__footer">
+        <Container>
+          <p>© 2025 EstateAgent | Find Your Dream Home</p>
+        </Container>
+      </footer>
+    </div>
+  )
+}
+
+export default SearchPage
